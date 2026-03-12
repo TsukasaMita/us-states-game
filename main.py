6e4973ba9ffd5037cd20afbb47539eef642677c8
TsukasaMita <tsukasa.mita@gmail.com>
2026-03-12 16:02:56 +0100

    feat: Add US States Game application and lock project dependencies.

---
+++ main.py
@@ -0,0 +1,35 @@
+from pandas.core.arrays.categorical import contains
+import turtle
+import pandas
+
+
+
+screen = turtle.Screen()
+screen.title("US States Game")
+
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+score = 0
+guessed_states = []
+
+state_data = pandas.read_csv("50_states.csv")
+all_states = state_data.state.to_list()
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Corret", prompt="What's another state's name?").title()
+
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state = state_data[state_data.state == answer_state]
+        t.goto(state.x.item(), state.y.item())
+        t.write(answer_state)
+
+
+        
+
+    
